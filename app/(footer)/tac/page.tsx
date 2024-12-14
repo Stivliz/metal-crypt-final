ab2506c8deb3abf920f2d34aa5f42629d778211b
stivliz <stivliz0909@gmail.com>
2024-12-13 20:15:37 -0500

    add: Footer, Tac and Pv con sus respectivos estilos para Mobile

---
+++ app/(footer)/tac/page.tsx
@@ -0,0 +1,35 @@
+import { tacItem } from "../items";
+import NavBar from "@/components/NavBar";
+import NavRoot from "@/components/NavRoot";
+import Footer from "@/components/Footer";
+
+interface Item {
+  id: number;
+  title: string;
+  description: string;
+}
+const TermsAndConditions = () => {
+  return (
+    <div>
+      <NavBar />
+      <NavRoot />
+      <div className="flex justify-center text-lg md:text-2xl mt-4 md:mt-9 mb-6 md:mb-12">
+        <h2 className="text-gray-300">Terms And Conditions</h2>
+      </div>
+      <div className="mb-9">
+        {tacItem.map((item: Item) => (
+          <div key={item.id} className="px-3 md:pl-32 pb-7">
+            <h5 className="md:text-lg text-gray-300">
+              {" "}
+              {item.id}. {item.title}
+            </h5>
+            <p className="text-gray-400">{item.description}</p>
+          </div>
+        ))}
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default TermsAndConditions;
--- app/news/page.tsx
+++ app/news/page.tsx
@@ -2,6 +2,8 @@
 import { useEffect, useState } from "react";
 import { Image } from "@nextui-org/react";
 import NewServices from "@/services/news.service";
+import NavBar from "@/components/NavBar";
+import NavRoot from "@/components/NavRoot";
 
 interface Data {
   _id: string;
@@ -43,26 +45,36 @@ const DataNews = () => {
   }
 
   return (
-    <div>
-      {data.length ? (
-        data.map((news: Data) => (
-          <div key={news._id}>
-            <h3 className="text-lg font-bold mt-2 text-white">{news.title}</h3>
-            <p>{news.img}</p>
-            <Image
-              isZoomed
-              src={news.img}
-              alt={news.title}
-              width={150}
-              height={150}
-              className="rounded-lg w-full h-auto object-cover transition-transform duration-200 ease-in-out hover:scale-110"
-            />
-            <p className="text-sm text-zinc-400">{news.description}</p>
-          </div>
-        ))
-      ) : (
-        <p className="text-gray-500">No news found.</p>
-      )}
+    <div className="h-[100vh]">
+      <NavBar />
+      <NavRoot />
+      <div className="flex justify-center flex-col items-center">
+        <div className="">
+          {data.length ? (
+            data.map((news: Data) => (
+              <div
+                key={news._id}
+                className="flex flex-col items-center border border-white w-max mx-16"
+              >
+                <h3 className="text-lg font-bold mt-2 text-white">
+                  {news.title}
+                </h3>
+                <Image
+                  isZoomed
+                  src={news.img}
+                  alt={news.title}
+                  width={150}
+                  height={150}
+                  className="rounded-lg w-full h-auto object-cover transition-transform duration-200 ease-in-out hover:scale-110"
+                />
+                <p className="text-sm text-zinc-400">{news.description}</p>
+              </div>
+            ))
+          ) : (
+            <p className="text-gray-500">No news found.</p>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
