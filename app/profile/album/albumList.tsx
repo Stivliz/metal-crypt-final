242794fe61b8c13784aec7aa6b79d4ee8c3d0408
Ghoul-JS <jmanuelgut170@gmail.com>
2024-12-26 11:48:06 -0500

    fix: albums on profile

--- app/profile/album/albumList.tsx
+++ app/profile/album/albumList.tsx
@@ -73,71 +73,66 @@ export function AlbumList() {
   }
 
   return (
-    <div>
-      <div className="flex items-center flex-col sm:flex sm:justify-between sm:flex-row">
-        {albums.length > 0 ? (
-          albums.map((album: Album) => (
-            <div
-              key={album._id}
-              className="max-w-max p-4 mb-2 bg-gray-900 rounded-lg shadow-md md:py-6 md:px-5"
-            >
-              {album.image && (
-                <button
-                  className=""
-                  onClick={() => toggleModal.openModalSong(album)}
-                >
-                  <Image
-                    isZoomed
-                    src={album.image}
-                    alt={album.name}
-                    width={150}
-                    height={150}
-                    className="rounded-lg w-full h-auto object-cover transition-transform duration-200 ease-in-out hover:scale-110"
-                  />
-                </button>
-              )}
-              <h3 className="text-lg font-bold mt-2 text-white">
-                {album.name}
-              </h3>
-              <p className="text-sm text-zinc-400">
-                {album.year} • {album.releaseType?.toUpperCase()}
-              </p>
-            </div>
-          ))
-        ) : (
-          <div className="col-span-4 p-4 bg-gray-900 rounded-lg shadow-md min-h-[200px] flex flex-col items-center justify-center">
-            <p className="text-gray-500">No albums found.</p>
-            <button
-              className="mt-4 bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-400"
-              onClick={toggleModal.openModalForm}
-            >
-              Create Album
-            </button>
-          </div>
-        )}
-        {albums.length > 0 && (
-          <div
-            className="p-4 bg-gray-900 rounded-lg shadow-md flex justify-center items-center cursor-pointer "
-            onClick={toggleModal.openModalForm}
-          >
-            <span className=" flex justify-center items-center text-5xl text-gray-600 w-36 h-56">
-              +
-            </span>
-          </div>
-        )}
-        {isModalSongOpen && selectedAlbum && (
-          <SongsAlbum
-            album={selectedAlbum}
-            closeModal={toggleModal.openModalSong}
-          />
-        )}
-        {isModalOpen && (
-          <CreateAlbum
-            closeModal={toggleModal.openModalForm}
-            onAlbumCreated={addAlbum}
-          />
-        )}
+ 
+    <div className="overflow-x-auto">
+  <div className="flex items-center gap-2">
+  {albums.length > 0 && (
+      <div
+        className="min-w-[150px] p-4 bg-gray-900 rounded-lg shadow-md flex justify-center items-center cursor-pointer mb-2"
+        onClick={toggleModal.openModalForm}
+      >
+        <span className="flex justify-center items-center text-5xl text-gray-600 w-36 h-56">
+          +
+        </span>
       </div>
-    </div>
+    )}
+    {albums.length > 0 ? (
+      albums.map((album: Album) => (
+        <div
+          key={album._id}
+          className="min-w-[200px] p-4 mb-2 bg-gray-900 rounded-lg shadow-md md:py-6 md:px-5"
+        >
+          {album.image && (
+            <button onClick={() => toggleModal.openModalSong(album)}>
+              <Image
+                isZoomed
+                src={album.image}
+                alt={album.name}
+                width={150}
+                height={150}
+                className="rounded-lg w-full h-auto object-cover transition-transform duration-200 ease-in-out hover:scale-110"
+              />
+            </button>
+          )}
+          <h3 className="text-lg font-bold mt-2 text-white">
+            {album.name}
+          </h3>
+          <p className="text-sm text-zinc-400">
+            {album.year} • {album.releaseType?.toUpperCase()}
+          </p>
+        </div>
+      )).reverse()
+    ) : (
+      <div className="col-span-4 p-4 bg-gray-900 rounded-lg shadow-md min-h-[200px] flex flex-col items-center justify-center">
+        <p className="text-gray-500">No albums found.</p>
+
+      </div>
+    )}
+
+    {isModalSongOpen && selectedAlbum && (
+      <SongsAlbum
+        album={selectedAlbum}
+        closeModal={toggleModal.openModalSong}
+      />
+    )}
+    {isModalOpen && (
+      <CreateAlbum
+        closeModal={toggleModal.openModalForm}
+        onAlbumCreated={addAlbum}
+      />
+    )}
+  </div>
+</div>
+
   );
 }
