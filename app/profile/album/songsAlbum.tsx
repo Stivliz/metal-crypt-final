0ff878d8f0b61a5fa79685acf9443ddd9c3c159a
stivliz <stivliz0909@gmail.com>
2024-12-28 16:42:54 -0500

    new changes

--- app/profile/album/songsAlbum.tsx
+++ app/profile/album/songsAlbum.tsx
@@ -5,7 +5,7 @@ const SongsAlbum = ({ album, closeModal }: any) => {
 
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-      <div className="bg-gray-800 text-white rounded-lg shadow-lg p-6 w-96 relative">
+      <div className="bg-gray-800 text-white rounded-lg shadow-lg p-6 w-60 md:w-96 relative">
         <button
           className="absolute top-2 right-2 text-gray-400 hover:text-gray-100"
           onClick={closeModal}
